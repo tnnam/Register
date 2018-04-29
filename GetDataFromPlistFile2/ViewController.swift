74d9e48769d55494b934e4f7c99ddd9019708d77
Tran Ngoc Nam <tranngocnam798@gmail.com>
2018-04-29 21:38:35 +0700

    Initial Commit

---
+++ GetDataFromPlistFile2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetDataFromPlistFile2
+//
+//  Created by Tran Ngoc Nam on 4/29/18.
+//  Copyright © 2018 Tran Ngoc Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
